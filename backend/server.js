f69c03edbabdd4fafea34168721aa0e3f9de948f
devendra <devendrarai884@gmail.com>
2026-09-25 13:01:05 +0530

    change

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,6 @@ const PORT = process.env.PORT || 5000;
 const startServer = async () => {
   try {
 
-    // Connect MongoDB
     await mongoose.connect(process.env.MONGO_URI);
 
     console.log("MongoDB connected successfully");
